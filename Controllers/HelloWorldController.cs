976f5af0bca730038646a71e147ad1113b8fe27c
dnh33 <danielhjermitslev@protonmail.com>
2022-07-25 14:59:22 +0000

    Finito

--- Controllers/HelloWorldController.cs
+++ Controllers/HelloWorldController.cs
@@ -5,15 +5,18 @@ namespace DoggoAppo.Controllers
 {
   public class HelloWorldController : Controller
   {
+
+    private static List<DogViewModel> dogs = new List<DogViewModel>();
+
     public IActionResult Index()
     {
-      DogViewModel doggo = new DogViewModel()
-      {
-        Name = "Sippo",
-        Age = 12
-      };
+      /*   DogViewModel doggo = new DogViewModel()
+        {
+          Name = "Sippo",
+          Age = 12
+        }; */
 
-      return View(doggo);
+      return View(dogs);
     }
 
     public IActionResult Create()
@@ -24,7 +27,8 @@ namespace DoggoAppo.Controllers
 
     public IActionResult CreateDog(DogViewModel dogViewModel)
     {
-      return View("Index");
+      dogs.Add(dogViewModel);
+      return RedirectToAction(nameof(Index));
     }
 
     public string Hello()
